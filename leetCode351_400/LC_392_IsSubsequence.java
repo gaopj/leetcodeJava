3b8d298b1f9bf2edb4a9efb3449ff3199bf96d97
gaopiji <gaopj@pku.edu.cn>
2018-01-03 10:51:53 +0800

    392. Is Subsequence

--- leetCode351_400/LC_392_IsSubsequence.java
+++ leetCode351_400/LC_392_IsSubsequence.java
@@ -7,4 +7,16 @@ public class LC_392_IsSubsequence {
 
 	}
 
+    public boolean isSubsequence(String s, String t) {
+        int spos=0;
+        int tpos=0;
+        int lens=s.length();
+        int lent = t.length();
+        while(spos<lens&&tpos<lent) {
+        	if(s.charAt(spos)==t.charAt(tpos)) {
+        		spos++;tpos++;
+        	}else tpos++;
+        }
+        return spos==lens;
+    }
 }
